801a08d175f2797caa41299a5c7028934fbc5268
podolskiya <145561835+podolskiya@users.noreply.github.com>
2023-12-04 22:22:01 +0100

    Update cleaning_data.py

--- cleaning_data.py
+++ cleaning_data.py
@@ -14,6 +14,22 @@ liberty_2022 = civil_liberties[civil_liberties['Year'] == 2021]
 # Merge three datasets by country to make one dataset, with inner join
 final_gdp = gdp_per_capita_2022.rename(columns={'2021': 'gdp_per_capita'})
 final_life_expectancy = life_expectancy_2022.rename(columns={'2021': 'life_expectancy'})
-print(liberty_2022.head(10))
-print(final_life_expectancy.head(10))
-print(final_gdp.head(10))
+merge_gdp_expectancy = pd.merge(final_gdp, final_life_expectancy)
+
+# Standardize country names
+liberty_2022.rename(columns={'Entity': 'Country Name'}, inplace = True)
+def normalize_name(name):
+    try:
+        return pycountry.countries.lookup(name).name
+    except LookupError:
+        return name
+
+liberty_2022['Country Name'] = liberty_2022['Country Name'].apply(normalize_name)
+merge_gdp_expectancy['Country Name'] = merge_gdp_expectancy['Country Name'].apply(normalize_name)
+
+liberty_2022.rename(columns={'Entity': 'Country Name'}, inplace = True)
+
+merged_liberty_life = pd.merge(liberty_2022, merge_gdp_expectancy, on='Country Name', how='inner')
+
+print(merged_liberty_life)
+merged_liberty_life.to_csv('ready_df.csv')
